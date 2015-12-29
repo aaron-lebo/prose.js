291a3c0acfa25948f9f4aac02786a24109da50be
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2015-12-28 23:49:56 -0600

    added JSON.stringify

--- src/prose.js
+++ src/prose.js
@@ -8,4 +8,4 @@ import parse from './parser';
 let file = fs.readFileSync(process.argv[2]),
     tokens = lex(file.toString()); 
 
-console.log(JSON.stringify(parse(tokens), null, 2));
+console.log(JSON.stringify(parse(tokens), null, 1));
