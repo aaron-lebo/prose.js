5e44fdb4336480985d7543ee6a24e2260ba24042
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2016-02-19 05:47:12 +0000

    added self-hosting

--- src/compiler.js
+++ src/compiler.js
@@ -1,121 +1,108 @@
+import Immutable from 'immutable';
 import escodegen from 'escodegen';
-
-function literal(val, raw) {
-    let node = {type: 'Literal', value: val};
-    if (raw) {
-        node.raw = raw;
-    }
+let literal = function (val, raw) {
+    let node = {
+        type: 'Literal',
+        value: val
+    };
+    (raw ? node.raw : null) = raw;
     return node;
-}
-
-function id(name) {
-    return {type: 'Identifier', name: name};
-} 
-
+};
+let id = function (name) {
+    return {
+        type: 'Identifier',
+        name: name
+    };
+};
 let vars = [];
-function lift(node, safe) {
-    if (Array.isArray(node)) {
-        return node.map(n => lift(n, safe));
-    } else if (node && typeof node == 'object' && !(node instanceof RegExp)) {
-        if (!safe && (node.type == 'VariableDeclaration')) {
+let lift = function (node, safe) {
+    if (Array.isArray(node))
+        (node.map(function (n) {
+            return lift(n, safe);
+        }))
+    else if (node && node === typeof && !(node instanceof RegExp)) {
+        if (!safe && node.type === 'VariableDeclaration') {
             vars.push(node);
             return node.declarations[0].id;
         }
-        Object.getOwnPropertyNames(node).forEach(n => {
-            node[n] = lift(node[n], n == 'body');
+        Object.getOwnPropertyNames(node).forEach(function (n) {
+            return node[n] = lift(node[n], n === 'body');
         });
     }
     return node;
-}
-
-let comments = [];
-function sweep(node, safe) {
-    if (Array.isArray(node)) {
-        return node.map(sweep);
-    } else if (node && typeof node == 'object' && !(node instanceof RegExp)) {
-        if (!safe && (node.type == '#')) {
-            comments.push(node);
-            return;
-        }
-        Object.getOwnPropertyNames(node).forEach(n => {
-            node[n] = sweep(node[n], n == 'body');
-        });
-    }
-    return node;
-}
-
-function statement(node, type) {
+};
+let statement = function (node, $throw) {
     let end = 'Statement';
-    if (node.type.endsWith(end)) {
+    if (node.type.endsWith(end))
         return node;
-    }
-    return {type: (type || 'Return') + end, argument: node};
-}
-
-function block(body, fun$) {
+    return {
+        type: ($throw ? 'Throw' : 'Return') + end,
+        argument: node
+    };
+};
+let block = function (body, fun$) {
+    body = fun$ === false ? body.map(convert) : body[0] === 'do' ? argsOf(body) : new Array(convert(body));
+    let i = 0;
     let $body = [];
-    if (fun$ == false) {
-        body = body.map(convert); 
-    } else {
-        body = body[0] == 'do' ? argsOf(body) : [convert(body)];
-    }
-    for (let i = 0; i < body.length; i++) {
+    while (i < body.length) {
         let $n = body[i];
-        $n = sweep(fun$ && i == body.length - 1 ? statement(lift($n)) : lift($n, true));
-        if (!$n) {
-            continue;
-        }
+        $n = fun$ && (i === body.length - 1 ? statement(lift($n)) : lift($n, true));
         $body = $body.concat(vars);
         vars = [];
-        if ($n.type == 'statements') {
-            $body = $body.concat($n.args);
-            continue;
-        }
-        let $statement = $n.type.endsWith('Expression') ? 
-            {type: 'ExpressionStatement', expression: $n} : 
-            $n;
-        $statement.leadingComments = comments;
-        comments = [];
-        $body.push($statement);
+        $body.push($n.type.endsWith('Expression') ? [
+            {
+                type: 'ExpressionStatement',
+                expression: $n
+            },
+            $n
+        ] : null);
     }
-    return fun$ == false ? $body : {type: 'BlockStatement', body: $body}; 
-}
-
-function argsOf(node, $convert=true) {
+    return fun$ === false ? $body : {
+        type: 'BlockStatement',
+        body: $body
+    };
+};
+let argsOf = function (node, $convert) {
     let args = node.slice(Array.isArray(node[1]) ? 1 : 2);
-    return $convert ? args.map(convert) : args; 
-}
-
-function fun(id, node) {
+    return $convert || true ? args.map(convert) : args;
+};
+let fun = function (id, node) {
     let body = node.slice(-1)[0];
-    body = block(body, 'return');
+    body = block(body, true);
     return {
-        type: 'Function' + (id ? 'Declaration' : 'Expression'),
+        type: 'Function' + id ? 'Declaration' : 'Expression',
         id: id && convert(id),
         params: node.slice(2, -1).map(convert),
         body: body
     };
-}   
- 
-function member(node, computed=false) {
-    let [obj, prop] = argsOf(node);
+};
+let member = function (node, computed) {
+    let [
+        obj,
+        prop
+    ] = argsOf(node);
     return {
         type: 'MemberExpression',
         object: obj,
         property: prop,
-        computed: computed 
+        computed: computed || true
     };
-}
- 
-function call(callee, args, $new) {
-    return {type: ($new ? 'New' : 'Call') + 'Expression', callee: callee, arguments: args};
-}
- 
-function object(node) {
+};
+let call = function (callee, args, $new) {
     return {
-        type: 'ObjectExpression', 
-        properties: argsOf(node, false).map(n => {
-            let [k, v] = argsOf(n);
+        type: ($new ? 'New' : 'Call') + 'Expression',
+        callee: callee,
+        arguments: args
+    };
+};
+let object = function (node) {
+    return {
+        type: 'ObjectExpression',
+        properties: argsOf(false).map(node, function (n) {
+            let [
+                k,
+                v
+            ] = argsOf(n);
             return {
                 type: 'Property',
                 key: k,
@@ -124,23 +111,24 @@ function object(node) {
             };
         })
     };
-}
-
-function variable(id, init) {
+};
+let variable = function (id, init) {
     return {
         type: 'VariableDeclaration',
-        declarations: [{
-            type: 'VariableDeclarator', 
+        declarations: new Array({
+            type: 'VariableDeclarator',
             id: id,
-            init: init 
-        }],
+            init: init
+        }),
         kind: 'let'
     };
-}
- 
-function expression(type) {
-    return (n, op) => {
-        let [left, right] = argsOf(n);
+};
+let expression = function (type) {
+    return function (n, op) {
+        let [
+            left,
+            right
+        ] = argsOf(n);
         return {
             type: type,
             operator: op || n[0],
@@ -148,114 +136,168 @@ function expression(type) {
             right: right
         };
     };
-}                
-
+};
 let assignment = expression('AssignmentExpression');
 let binary = expression('BinaryExpression');
 let logical = expression('LogicalExpression');
-
-let nodes = {
-    '#': n => ({type: '#'}),
-    boolean: n => literal(null), 
-    name: n => id(n[2]),
-    number: n => literal(parseFloat(n[2])),
-    string: n => literal(null, { 
-          content: n[2],
-          precedence: escodegen.Precedence.Primary
-    }),
-    regex: n => literal(RegExp.apply(null, argsOf(n, false))),
-    new: n => {
-        let [callee, args] = argsOf(n);
-        return call(callee, args ? [args]: [], true);
+let nodes = Immutable.HashMap({
+    boolean: function (n) {
+        return literal(null);
     },
-    not: n => ({
-        type: 'UnaryExpression',
-        operator: '!',
-        prefix: true,
-        argument: argsOf(n)[0]
-    }),
-    function: n => fun(null, n), 
-    return: n => statement(convert(n[2])),
-    throw: n => statement(convert(n[2]), 'Throw'),
-    '?': n => {
-        let [test, con, alt] = argsOf(n, false);
+    name: function (n) {
+        return id(n[2]);
+    },
+    number: function (n) {
+        return literal(parseFloat(n[2]));
+    },
+    string: function (n) {
+        return literal(null, {
+            content: n[2],
+            precedence: escodegen.Precedence.Primary
+        });
+    },
+    regex: function (n) {
+        return RegExp.apply(null, literal(argsOf(n, false)));
+    },
+    new: function (n) {
+        let [
+            callee,
+            args
+        ] = argsOf(n);
+        return call(callee, args ? new Array(args) : [], true);
+    },
+    not: function (n) {
+        return {
+            type: 'UnaryExpression',
+            operator: '!',
+            prefix: true,
+            argument: argsOf(n)[0]
+        };
+    },
+    function: function (n) {
+        return fun(null, n);
+    },
+    return: function (n) {
+        return statement(convert(n[2]));
+    },
+    throw: function (n) {
+        return statement(convert(n[2]), true);
+    },
+    '?': function (n) {
+        let [
+            test,
+            con,
+            alt
+        ] = argsOf(n, false);
         test = convert(test);
-        let unsafe = [con, alt].filter(n => n && n[0] == 'do').length > 0;
+        let unsafe = [
+            con,
+            alt
+        ].filter(function (n) {
+            return n && n[0] === 'do';
+        }).length > 0;
         let fun = unsafe ? block : convert;
         con = fun(con);
         alt = alt ? fun(alt) : null;
-        let $statement = [con, alt].filter(n => n && n.type.endsWith('Statement')).length > 0; 
+        let $statement = [
+            con,
+            alt
+        ].filter(function (n) {
+            return n && n.type.endsWith('Statement');
+        }).length > 0;
         return {
-            type: $statement ? 'IfStatement' : 'ConditionalExpression', 
+            type: $statement ? 'IfStatement' : 'ConditionalExpression',
             test: test,
             consequent: con,
-            alternate: alt || ($statement ? null : literal(null)) 
-        }
-    },      
-    for: n => {
-       let body = n.slice(-1)[0];
-       return {
+            alternate: alt || ($statement ? null : literal(null))
+        };
+    },
+    for: function (n) {
+        let body = n.slice(-1)[0];
+        return {
             type: 'WhileStatement',
             test: convert(n[2]),
             body: block(body)
         };
-    },    
-    '&': n => logical(n, '&&'), 
-    '|': n => logical(n, '||'), 
-    at: n => member(n, true),
-    import: n => {
+    },
+    '&': function (n) {
+        return logical(n, '&&');
+    },
+    '|': function (n) {
+        return logical(n, '||');
+    },
+    at: function (n) {
+        return member(n, true);
+    },
+    import: function (n) {
         return {
-            type: 'statements', 
-            args: argsOf(n, false).map($n => {
-                let [spec, source] = argsOf($n);
-                return {
-                    type: 'ImportDeclaration',
-                    specifiers: [{type: 'ImportDefaultSpecifier', id: spec}],
-                    source: source 
-                };
-            })
+            type: 'VariableDeclaration',
+            declarations: n.args.map(function (n) {
+                let [
+                    left,
+                    right
+                ] = $n.args.map(convert);
+                return variable(left, call(id('require'), new Array(right)));
+            }),
+            kind: 'let'
         };
     },
     '+': binary,
     '-': binary,
-    '==': n => binary(n, '==='),
-    '!=': n => binary(n, '!=='),
+    '==': function (n) {
+        return binary(n, '===');
+    },
+    '!=': function (n) {
+        return binary(n, '!==');
+    },
     '<': binary,
-    '>': binary,
-    instanceof: n => binary(n, 'instanceof'),
     Obj: object,
-    Array: n => ({type: 'ArrayExpression', elements: argsOf(n)}),      
-    HashMap: n => call(id('Immutable.HashMap'), [object(n)]),
-    OrderedMap: n => call(id('Immutable.OrderedMap'), [object(n)]),    
-    '.': n => member(n), 
-    '=': n => variable.apply(null, argsOf(n)),
-    ':=': n => assignment(n, '='),
+    Array: function (n) {
+        return {
+            type: 'ArrayExpression',
+            elements: argsOf(n, n)
+        };
+    },
+    HashMap: function (n) {
+        return call(id('Immutable.HashMap'), new Array(object(n)));
+    },
+    OrderedMap: function (n) {
+        return call(id('Immutable.OrderedMap'), new Array(object(n)));
+    },
+    '.': member,
+    '=': function (n) {
+        return variable.apply(null, argsOf(n));
+    },
+    ':=': function (n) {
+        return assignment(n, '=');
+    },
     '+=': assignment,
-    ':': n => {     
-        if (n[0] == 'default' || n[2][2] == 'default') {
-            return {type: 'ExportDefaultDeclaration', declaration: fun.apply(null, argsOf(n[3], false))};
-        }
+    ':': function (n) {
+        if (n[0] === 'default' || n[2][2] === 'default')
+            return {
+                type: 'ExportDefaultDeclaration',
+                declaration: fun.apply(null, argsOf(n[3], false))
+            };
         let args = argsOf(n, false);
         args.splice(1, 0, {});
-        return statement(convert(args)); 
+        return statement(convert(args));
     }
-}
-
-function convert(node) {
-    let head = node[0]; 
+});
+let convert = function (node) {
+    let head = node[0];
     let parser = nodes[head] || nodes[head[2]];
-    return parser ? parser(node) : 
-        call(Array.isArray(head) ? convert(head) : id(head), argsOf(node));
-}
-
+    return parser ? parser(node) : call(Array.isArray(head) ? convert(head) : id(head), argsOf(node));
+};
 export default function compile(ast) {
-    let first = ast[0].slice(-1)[0];
-    return (/^[/s]?#!/.exec(first) ? first + '\n' : '') + escodegen.generate({
+    return escodegen.generate({
         type: 'Program',
-        body: [{
+        body: new Array({
             type: 'ImportDeclaration',
-            specifiers: [{type: 'ImportDefaultSpecifier', id: id('Immutable')}],
-            source: literal('immutable') 
-        }].concat(block(ast, false))
-    }, {verbatim: 'raw'});
+            specifiers: new Array({
+                type: 'ImportDefaultSpecifier',
+                id: id('Immutable')
+            }),
+            source: literal('immutable')
+        }).concat(block(ast, false))
+    }, { verbatim: 'raw' });
 }
